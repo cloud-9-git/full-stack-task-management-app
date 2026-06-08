50227eab08576ced50d980b43fa681346f382f6e
cloud-9-git <reum.celeste.koo@gmail.com>
2026-06-08 16:47:25 +0900

    chore: make frontend API base URL configurable

--- frontend/src/components/api.jsx
+++ frontend/src/components/api.jsx
@@ -10,7 +10,7 @@ export const ENDPOINTS={
 }
 
 export const instance = axios.create({
-    baseURL:"http://127.0.0.1:8000/"
+    baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/"
 })
 
 instance.interceptors.request.use(config=>{
